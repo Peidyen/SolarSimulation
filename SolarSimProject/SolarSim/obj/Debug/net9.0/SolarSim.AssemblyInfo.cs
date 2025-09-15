03a762745402fc2c7ba820e942f5a53938a6fa84
Peidyen <mitch.ronco@gmail.com>
2025-09-15 18:20:17 -0400

    added gitignore and a couple web assembly updates

--- SolarSimProject/SolarSim/obj/Debug/net9.0/SolarSim.AssemblyInfo.cs
+++ SolarSimProject/SolarSim/obj/Debug/net9.0/SolarSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolarSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34739b485c374bf6cb094c7da3cb49321ca535ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolarSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolarSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
